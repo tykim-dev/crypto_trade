b201a409b3af5f927c60d00dab7b7332986a60d2
김태연 <xodus776@nate.com>
2024-10-30 12:21:26 +0900

    수정

---
+++ next.config.js
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first")
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // domains: ['http://k.kakaocdn.net', 'http://k.kakaocdn.net'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'k.kakaocdn.net',
+        port: '',
+        pathname: '/**/*',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/300/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // experimental: {
+  //   missingSuspenseWithCSRBailout: false,
+  // },
+}
+
+module.exports = nextConfig
