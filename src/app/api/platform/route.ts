bf6da8166fc58af7238f5837fa1c271f69b093f0
김태연 <xodus776@nate.com>
2024-11-01 16:22:58 +0900

    수정

--- src/app/api/platform/route.ts
+++ src/app/api/platform/route.ts
@@ -6,15 +6,33 @@ import UserModel from '@/app/models/user.model';
 import UserApiModel from '@/app/models/user_api.model';
 import bcrypt from "bcrypt";
 import trade from './bitget/route';
+import { useState } from 'react';
+import { isEmpty } from 'lodash';
 
 export async function POST(request: NextRequest) {
   await connectDB();
 
+  let result = {};
+
   const body = await request.json();;
   
+  // 사용자 정보 조회
   const userInfo = await UserModel.findOne({apiKey: body.apiKey});
-  const userApiInfo = await UserApiModel.findOne({user: userInfo._id}, {apiInfos: {$elemMatch: {platform: body.platform}}})
-  const bInfo = await trade(userApiInfo.apiInfos[0]);
 
-  return NextResponse.json(bInfo)
+  // 사용자 정보가 있을 경우
+  if(isEmpty(userInfo)) {
+    result = {code: '99999', msg: '사용자 정보가 없습니다.'};
+  } else {
+    const userApiInfo = await UserApiModel.findOne({user: userInfo._id}, {apiInfos: {$elemMatch: {platform: body.platform}}})
+
+    // 사용자의 거래소 API가 있을 경우
+    if(isEmpty(userApiInfo) || isEmpty(userApiInfo.apiInfos)) {
+      result = {code: '99999', msg: '등록된 거래소 API가 없습니다.'};
+    } else {
+      const resultProc = await trade(userApiInfo.apiInfos[0]);
+      result = resultProc;
+    }
+  }
+
+  return NextResponse.json(result)
 }
